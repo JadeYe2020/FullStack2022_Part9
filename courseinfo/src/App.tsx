55086a389118fa317f3fcf13545fea04fbd4cca1
Jade Ye <w0430394@nscc.ca>
2022-09-05 11:04:06 -0300

    Finish 9.15

--- courseinfo/src/App.tsx
+++ courseinfo/src/App.tsx
@@ -1,20 +1,114 @@
 const App = () => {
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CoursePartWithDesc extends CoursePartBase {
+    description: string;
+  }
+
+  interface CourseNormalPart extends CoursePartWithDesc {
+    type: "normal";
+  }
+
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartWithDesc {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  interface CourseSpecialPart extends CoursePartWithDesc {
+    type: "special";
+    requirements: string[];
+  }
+
+  type CoursePart =
+    | CourseNormalPart
+    | CourseProjectPart
+    | CourseSubmissionPart
+    | CourseSpecialPart;
+
   const Header = ({ name }: { name: string }) => {
     return <h1>{name}</h1>;
   };
 
-  interface CoursePart {
-    name: string;
-    exerciseCount: number;
-  }
+  /**
+   * Helper function for exhaustive type checking
+   */
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  const Part = ({ coursePart }: { coursePart: CoursePart }) => {
+    switch (coursePart.type) {
+      case "normal":
+        return (
+          <p>
+            <strong>
+              {coursePart.name} {coursePart.exerciseCount}
+            </strong>
+            <br />
+            <em>{coursePart.description}</em>
+          </p>
+        );
+        break;
+      case "groupProject":
+        return (
+          <p>
+            <strong>
+              {coursePart.name} {coursePart.exerciseCount}
+            </strong>
+            <br />
+            project exercises {coursePart.groupProjectCount}
+          </p>
+        );
+        break;
+      case "submission":
+        return (
+          <p>
+            <strong>
+              {coursePart.name} {coursePart.exerciseCount}
+            </strong>
+            <br />
+            <em>{coursePart.description}</em>
+            <br />
+            submit to {coursePart.exerciseSubmissionLink}
+          </p>
+        );
+        break;
+      case "special":
+        return (
+          <p>
+            <strong>
+              {coursePart.name} {coursePart.exerciseCount}
+            </strong>
+            <br />
+            <em>{coursePart.description}</em>
+            <br />
+            required skills: {coursePart.requirements.join(", ")}
+          </p>
+        );
+        break;
+      default:
+        return assertNever(coursePart);
+        break;
+    }
+  };
 
   const Content = (props: { courseParts: CoursePart[] }) => {
     return (
       <div>
         {props.courseParts.map((item) => (
-          <p key={item.name}>
-            {item.name} {item.exerciseCount}
-          </p>
+          <Part key={item.name} coursePart={item} />
         ))}
       </div>
     );
@@ -30,18 +124,40 @@ const App = () => {
   };
 
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special",
     },
   ];
 
